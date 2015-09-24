c6aad707855f7e50009e9fa0b74dda4ce528cb72
broadreach <broadreaching79@gmail.com>
2015-09-24 18:09:14 -0400

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -1,12 +1,19 @@
 //Note to self: the style property does not include the styles in an external CSS file, only
-//inline styles. So 'display' is initially set to 'none' in CSS.
+//inline styles. So 'display' is initially set to 'none' in CSS, but don't test this until the 
+//script makes a change and the new style becomes an inline style.
 
 function dropDownMenus() {
+    
     var menuItems = document.getElementsByClassName('dropdown');
+    
     for (var i = 0; i < menuItems.length; i++) {
+        
+        //Wrap 'click' function inside an IIFE in order to be able to pass each event listener a unique value for menuItems[i].
+        
         menuItems[i].addEventListener('click', (function(currentItem) { 
-            return function(e) { //Wrap function inside an IIFE in order to be able to pass each 
-                                 //event listener a unique value for menuItems[i].
+            
+            return function(e) { 
+                
                 for (var i = 0; i < menuItems.length; i++) {
                     if (menuItems[i] == currentItem) {
                         continue;
@@ -15,6 +22,7 @@ function dropDownMenus() {
                         menuItems[i].children[1].style.display = 'none';
                     }
                 };
+                
                 if (currentItem.children[1].style.display == 'block') {
                     currentItem.children[1].style.display = 'none';
                 }
@@ -25,13 +33,16 @@ function dropDownMenus() {
                 }
             };
         })(menuItems[i]));
+        
+        //Hides each item if user clicks anywhere else on the page, including other menu items.
+        
         document.addEventListener('click', (function(currentItem) { 
             return function() {
                     if (currentItem.children[1].style.display == 'block') {    
                         currentItem.children[1].style.display = 'none';
                     }
             };
-        })(menuItems[i])); //Hides each item if user clicks anywhere else on the page, including other menu items.
+        })(menuItems[i])); 
     }
 }
 
